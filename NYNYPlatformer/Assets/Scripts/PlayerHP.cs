05351b402bb431a24bf9533d0d7a16c07bf9ed86
Andrew Nelson <20.andrew.nelson@ksd.org>
2018-12-07 09:05:43 -0800

    Apartment Finish and Tutorial

--- NYNYPlatformer/Assets/Scripts/PlayerHP.cs
+++ NYNYPlatformer/Assets/Scripts/PlayerHP.cs
@@ -9,7 +9,7 @@ public class PlayerHP : MonoBehaviour
     public int HP = 3;
     public Text HPText;
     public string GameOver;
-   
+    float timer = 0;
     
     
     
@@ -19,6 +19,7 @@ public class PlayerHP : MonoBehaviour
         // make timer for on trigger so you have to wait 1 second.
         HP = PlayerPrefs.GetInt("Health");
         HPText.GetComponent<Text>().text = "Health " + HP;
+        timer += Time.deltaTime;
         
     }
     void Start()
@@ -29,59 +30,41 @@ public class PlayerHP : MonoBehaviour
         HPText.GetComponent<Text>().text = "Health: " + HP;
 
     }
-     void OnTriggerEnter2D(Collider2D collision)
+    void OnTriggerEnter2D(Collider2D collision)
     {
         // if more than 1 Health minus health
         float yVelocity = GetComponent<Rigidbody2D>().velocity.y;
-        if (collision.gameObject.tag == "Enemy" && HP >= 2 && yVelocity >= 0)
+        if (collision.gameObject.tag == "Enemy" && HP >= 2 && timer >= 1)
         {
 
             //  HP--;
 
             PlayerPrefs.SetInt("Health", HP - 1);
-
-
-
-
-
-
-        }
-        if (collision.gameObject.tag == "Health" && HP <= 4)
-        {
-            PlayerPrefs.SetInt("Health", HP + 1);
             Destroy(collision.gameObject);
+
+
+
         }
-        if (collision.gameObject.tag == "Health" && HP >= 5)
-        {
-            Destroy(collision.gameObject);
-        }
-        // if less than 1 Health lose screen
-        else if (collision.gameObject.tag == "Enemy" && HP <= 1 && yVelocity >= 0)
+         else if (collision.gameObject.tag == "Enemy" && HP <= 1 && yVelocity >= 0)
         {
 
             SceneManager.LoadScene(GameOver);
-
+          
         }
-
-
-
     }
 
 
 
-    void OnCollisionEnter2D(Collision2D collision)
+        void OnCollisionEnter2D(Collision2D collision)
     {
         // if more than 1 Health minus health
         float yVelocity = GetComponent<Rigidbody2D>().velocity.y;
-        if (collision.gameObject.tag == "Enemy" && HP >= 2 && yVelocity >=0)
+        if (collision.gameObject.tag == "Enemy" && HP >= 2 && timer >= 1)
         {
 
           //  HP--;
            
             PlayerPrefs.SetInt("Health", HP - 1);
-
-          
-            
             
             
         
---
+++ NYNYPlatformer/Assets/Scripts/Tutorial.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tutorial : MonoBehaviour {
+    float timerMove;
+    float timerJump;
+    public GameObject MoveCanvas;
+    public GameObject JumpCanvas;
+    bool JumpCanvasOn = false;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        timerMove += Time.deltaTime;
+        if(timerMove >=2)
+        {
+            MoveCanvas.SetActive(false);
+        }
+        if (JumpCanvasOn)
+        {
+            timerJump += Time.deltaTime;
+        }
+        if (timerJump >= 2)
+        {
+            JumpCanvas.SetActive(false);
+        }
+	}
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.name == ("Tutorial"))
+        {
+            MoveCanvas.SetActive(false);
+            JumpCanvas.SetActive(true);
+
+            JumpCanvasOn = true;
+            Destroy(collision.gameObject);
+        }
+    }
+}
